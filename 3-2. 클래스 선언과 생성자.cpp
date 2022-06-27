4d6f7832a26be03166d2fa59c43ee1ea50badfe2
DDING <103184052+DDingshark@users.noreply.github.com>
2022-06-27 16:04:07 +0900

    Update 3-2. 클래스 선언과 생성자.cpp

--- 3-2. 클래스 선언과 생성자.cpp
+++ 3-2. 클래스 선언과 생성자.cpp
@@ -3,14 +3,17 @@ using namespace std;
 class USERDATA
 {
 public:
-//���� ������. private, protected���� ������ �����ϴ°͵����� ���⼱ public�������̶� �ǹ�.
+//접근 지시자. private, protected같이 접근을 제한하는것도있음 여기선 public모두허용이란 의미.
+//public : 맴버에 관한 모든외부전급을 허용.
+//protect : 맴버의관한 모든 외부접근을 차단 (상속관계의 파생클래스는 허용)
+//private : 상속관예의 파생클래스까지도 차단 // 기본값입니당.
 	int nAge;
 	char szName[32];
 
 	void print()
 	{
 		printf("%d, %s\n", nAge, szName);
-		//this->nAge�� ����ok print�Լ������� Ŭ������ �ɹ��� �����ϴ°� ������ �׳ɽᵵ �����մϴ�.
+		//this->nAge라 서도ok print함수가속한 클래스의 맴버에 접근하는것 임으로 그냥써도 무관합니당.
 
 	}
 };
@@ -20,19 +23,19 @@ class CTest
 {
 public:
 	CTest()
-	//������ �Լ� ����
-	// �� �Լ��� '�����'�� ��ü�� �����ϸ� �ڵ������� ȣ��˴ϴ�.
+	//생성자 함수 정의
+	// 이 함수는 '사용자'가 객체를 선언하면 자동적으로 호출됩니당.
 	{
-		//�ν��Ͻ��� �����Ǹ� �ڵ������� �ʱ�ȭ �մϴ�.
-		cout << "CTest() : ������ �Լ�" << endl;
+		//인스턴스가 생성되면 자동적으로 초기화 합니다.
+		cout << "CTest() : 생성자 함수" << endl;
 		m_nData = 10;
 	}
 	~CTest()
 	{
-		cout << "~CTest() �Ҹ��� �Լ�" << endl;
+		cout << "~CTest() 소멸자 함수" << endl;
 	}
 
-	// �����ڿ� �Ҹ����� ����ū Ư¡ 1. ȣ������� ����, 2. ��ȯ��X
+	// 생성자와 소멸자의 가장큰 특징 1. 호출시점의 자유, 2. 반환값X
 
 	int m_nData;
 
@@ -46,13 +49,13 @@ class C_11Test
 {
 public:
 	int n_main = 10;
-	//�̷������� ����� ���ÿ� �ʱ�ȭ�� ���� ( c11���ĺ���)
+	//이런식으로 선언과 동시에 초기화도 가능 ( c11이후부터)
 
 };
 
 
 CTest t_upper_main;
-//�̷������� main�Լ����� ���� ���� �� �� �ִ�.
+//이런식으로 main함수보다 위에 선언 할 수 있다.
 int main()
 {
 	cout << "main start" << endl;
@@ -64,11 +67,11 @@ int main()
 
 
 	CTest t;
-	//������ ȣ�� 
+	//생성자 호출 
 	t.PrintData();
 
 
 	cout << "main end" << endl;
 	return 0;
 }
-//�Ҹ������ main�Լ� �ڷ� �Ѿ� �� ���� �ִ�.
+//소멸시점이 main함수 뒤로 넘어 갈 수도 있다.
